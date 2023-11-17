a56b258b37e4b59e7ad128b1a4fcc37e30cb576f
EmnAsPW <emonsourov@gmail.com>
2023-11-17 19:44:46 +0600

    testing without pagination

--- src/auth/login-user.guards.ts
+++ src/auth/login-user.guards.ts
@@ -8,7 +8,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const LoginUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    console.log('.....', ctx.getContext().req.user);
+    //console.log('.....', ctx.getContext().req.user);
 
     return ctx.getContext().req.user;
   },
